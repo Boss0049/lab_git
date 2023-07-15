1ea14a29c4579cbc8f7c88d56634cb9b27ef7478
phachara <oat19411@hotmail.com>
2023-07-15 19:50:56 +0700

    Js modify

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const returnString = (string) => {
   console.log(string)
+  console.log(string.length)
+  console.log(string.split("o"));
+  console.log("=========================");
 };
 
 returnString("helloWolrd")
